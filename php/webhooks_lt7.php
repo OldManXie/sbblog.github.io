be37d73757409fe05aa013722db4aae47c0fe185
OldManXie <xie950513@gmail.com>
2021-03-25 15:30:17 +0800

    Site updated: 2021-03-25 15:30:16

--- php/webhooks_lt7.php
+++ php/webhooks_lt7.php
@@ -7,7 +7,7 @@ if ($signature) {
     $hash = "sha1=".hash_hmac('sha1', $HTTP_RAW_POST_DATA, $secret);
     if (strcmp($signature, $hash) == 0) {
         //shell_exec执行shell命令，2>&1用于标准错误重定向到标准输出，也就是脚本执行错误时也能返回错误信息
-        echo shell_exec("/alidata/package/python3/bin/python3 /personal/www/www.sbblog.top/php/remove_blank.py && cd /personal/www/www.sbblog.top && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+        echo shell_exec("cd /personal/www/www.sbblog.top && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1 &&/alidata/package/python3/bin/python3 /personal/www/www.sbblog.top/php/remove_blank.py");
         exit(date('Y-m-d H:i:s'));
     }
 }
