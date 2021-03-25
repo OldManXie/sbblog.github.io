8463389c35016994ecb0cfa4efb78f91f0cf8124
OldManXie <xie950513@gmail.com>
2021-03-25 13:19:56 +0800

    Site updated: 2021-03-25 13:19:49

--- php/webhooks_lt7.php
+++ php/webhooks_lt7.php
@@ -8,9 +8,10 @@ if ($signature) {
     $hash = "sha1=".hash_hmac('sha1', $HTTP_RAW_POST_DATA, $secret);
     if (strcmp($signature, $hash) == 0) {
         //shell_exec执行shell命令，2>&1用于标准错误重定向到标准输出，也就是脚本执行错误时也能返回错误信息
-        echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+        echo exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
         exit();
     }
 }
 http_response_code(404);
 ?>
+
